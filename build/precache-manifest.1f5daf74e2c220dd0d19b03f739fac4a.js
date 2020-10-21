9b9d79594b54b362733325e6d394cd56dc724330
Shahariar Rahman Sajib <61103469+shahsajib95@users.noreply.github.com>
2020-10-21 19:41:07 +0600

    add

---
+++ build/precache-manifest.1f5daf74e2c220dd0d19b03f739fac4a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e111385c8a82a4bd25facb2c597dca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7d5e75b55d585c72bf6",
+    "url": "/static/css/2.f0e23f32.chunk.css"
+  },
+  {
+    "revision": "fbb3bbb48227e5950c61",
+    "url": "/static/css/main.44af6f5a.chunk.css"
+  },
+  {
+    "revision": "c7d5e75b55d585c72bf6",
+    "url": "/static/js/2.b9c0f406.chunk.js"
+  },
+  {
+    "revision": "a89043df82952537733028209276149a",
+    "url": "/static/js/2.b9c0f406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb3bbb48227e5950c61",
+    "url": "/static/js/main.847d2de1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c530f67977c1e9f641f192bd24f3ac9b",
+    "url": "/static/media/Sajek.c530f679.png"
+  },
+  {
+    "revision": "6d0300f1917fd2d4b46acbd981db432e",
+    "url": "/static/media/Sreemongol.6d0300f1.png"
+  },
+  {
+    "revision": "32162f64d326b339d4a75b6d64d9e6c7",
+    "url": "/static/media/image.32162f64.png"
+  },
+  {
+    "revision": "978671e9439715f03dde2ef75ab3db8c",
+    "url": "/static/media/sundorbon.978671e9.png"
+  }
+]);
